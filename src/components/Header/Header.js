4195718c3e4e366cf5be80d0a6cce28df2810fc4
nasirullahnu <nasirullah878@gmail.com>
2022-10-23 02:03:03 +0600

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <img src={logo} alt="" />
             <div>
                 <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
+                <Link to="/orders">Order</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
             </div>
